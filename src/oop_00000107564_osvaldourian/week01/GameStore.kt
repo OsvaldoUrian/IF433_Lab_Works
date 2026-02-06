443a67b41326e5054c64475e1b098e4676c120af
Osvaldo Urian <osvaldourian28@gmail.com>
2026-02-06 19:42:09 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000107564_osvaldourian/week01/GameStore.kt
+++ src/oop_00000107564_osvaldourian/week01/GameStore.kt
@@ -1,4 +1,17 @@
-package oop_00000107564_osvaldourian.week01
+package oop_00000107564_osvaldourian.week1
 
-class GameStore {
+fun main() {
+    val gameTitle = "Elden Ring"
+    val price = 750000
+
+    val finalPrice = calculateDiscount(price)
+
+    println(gameTitle)
+    println(finalPrice)
 }
+
+fun calculateDiscount(price: Int) =
+    if (price > 500000)
+        (price * 0.8).toInt()
+    else
+        (price * 0.9).toInt()
