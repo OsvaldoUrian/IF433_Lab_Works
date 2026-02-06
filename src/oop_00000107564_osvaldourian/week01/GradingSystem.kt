731c96e01160c90cc4a8383e617b12916927dd82
Osvaldo Urian <osvaldourian28@gmail.com>
2026-02-06 19:05:19 +0700

    week01: add calculateStatus function

--- src/oop_00000107564_osvaldourian/week01/GradingSystem.kt
+++ src/oop_00000107564_osvaldourian/week01/GradingSystem.kt
@@ -1,11 +1,14 @@
 package oop_00000107564_osvaldourian.week01
 
+fun calculateStatus(score: Int) =
+    if (score > 75) "Lulus" else "Tidak Lulus"
+
 fun main() {
     val name = "Osvaldo"
     val score = 80
 
-   println("Nama: $name, Nilai: %score")
-    // Tambahkan di bawah println
+    println("Nama: $name, Nilai: $score")
+
     val grade = when (score) {
         in 90..100 -> "A"
         in 80..89  -> "B"
@@ -14,3 +17,5 @@ fun main() {
     }
 
     println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
+}
