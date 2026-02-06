e0ead7e29382ab17da417cf74ec056b2c3a91382
Osvaldo Urian <osvaldourian28@gmail.com>
2026-02-06 18:55:11 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000107564_osvaldourian/week01/GradingSystem.kt
@@ -0,0 +1,12 @@
+package oop_00000107564_osvaldourian.week01
+
+fun main() {
+    var name: String = "Osvaldo"
+    var score: Int = 85
+
+    if (score >= 75) {
+        println("Student " + name + " Passed")
+    } else {
+        println("Student " + name + " Failed")
+    }
+}
