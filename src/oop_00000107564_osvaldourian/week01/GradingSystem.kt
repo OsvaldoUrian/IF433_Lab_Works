8ac0ae85b1665a7b120a2f1de7db9aa593dcf735
Osvaldo Urian <osvaldourian28@gmail.com>
2026-02-06 18:59:31 +0700

    week01: refactor to use val and string templates

--- src/oop_00000107564_osvaldourian/week01/GradingSystem.kt
+++ src/oop_00000107564_osvaldourian/week01/GradingSystem.kt
@@ -1,12 +1,7 @@
 package oop_00000107564_osvaldourian.week01
 
 fun main() {
-    var name: String = "Osvaldo"
-    var score: Int = 85
+    val name = "Osvaldo"
+    val score = 80
 
-    if (score >= 75) {
-        println("Student " + name + " Passed")
-    } else {
-        println("Student " + name + " Failed")
-    }
-}
+   println("Nama: $name, Nilai: %score")
