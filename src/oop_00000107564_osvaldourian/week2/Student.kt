be379e739c0fdeca3de1c3ff3af2b7e3348e3b06
Osvaldo Urian <osvaldourian28@gmail.com>
2026-02-11 10:39:43 +0700

    week02: add warning validation in init block

--- src/oop_00000107564_osvaldourian/week2/Student.kt
+++ src/oop_00000107564_osvaldourian/week2/Student.kt
@@ -1,7 +1,14 @@
 package oop_00000107564_osvaldourian.week2
 
-class Student(
-    val nim: String,
-    var name: String,
-    var major: String
-)
+class Student(val name: String, val nim: String, var major: String) {
+
+    init {
+        // Validasi Sederhana: Cek panjang NIM
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM $nim yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
+}
