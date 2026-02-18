522b872a4d33d25b0e889ad4eeebbb59f79ab6e0
Osvaldo Urian <osvaldourian28@gmail.com>
2026-02-18 10:42:00 +0700

    week03: add private property

--- src/oop_00000107564_osvaldourian/week3/Employee.kt
+++ src/oop_00000107564_osvaldourian/week3/Employee.kt
@@ -11,4 +11,16 @@ class Employee(val name: String) {
                 field = value
             }
         }
+
+    // Hanya bisa diakses di dalam class ini saja
+    private var performanceRating: Int = 3
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
 }
--- src/oop_00000107564_osvaldourian/week3/Main.kt
+++ src/oop_00000107564_osvaldourian/week3/Main.kt
@@ -2,6 +2,13 @@ package oop_00000107564_osvaldourian.week3
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = -100   // ini akan membuat program meledak
+
+    println("--- Test Salary ---")
+    e.salary = -100
+    e.salary = 5000
+
+    println("\n--- Test Performance ---")
+    e.printStatus()
+    e.increasePerformance()
+    e.printStatus()
 }
