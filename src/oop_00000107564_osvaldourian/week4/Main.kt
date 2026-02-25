90b76a54f6b94afb2edbcbe0338a39005fdf9786
Osvaldo Urian <osvaldourian28@gmail.com>
2026-02-25 10:48:07 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000107564_osvaldourian/week4/Main.kt
@@ -0,0 +1,15 @@
+package oop_00000107564_osvaldourian.week4
+
+fun main() {
+
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
