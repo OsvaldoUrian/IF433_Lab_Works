527dfe3539ca20dbac22ed162ba41f2500ce9c98
Osvaldo Urian <osvaldourian28@gmail.com>
2026-03-04 10:54:18 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000107564_osvaldourian/week5/Main.kt
+++ src/oop_00000107564_osvaldourian/week5/Main.kt
@@ -30,4 +30,16 @@ fun main() {
 
         println("----------------------------")
     }
+
+    println("\n=== UJI MATHHELPER (OVERLOADING) ===")
+    val math = MathHelper()
+
+    val luasPersegi = math.hitungLuas(5)
+    println("Luas Persegi (sisi=5): $luasPersegi")
+
+    val luasPersegiPanjang = math.hitungLuas(4, 7)
+    println("Luas Persegi Panjang (4x7): $luasPersegiPanjang")
+
+    val luasLingkaran = math.hitungLuas(3.5)
+    println("Luas Lingkaran (r=3.5): $luasLingkaran")
 }
