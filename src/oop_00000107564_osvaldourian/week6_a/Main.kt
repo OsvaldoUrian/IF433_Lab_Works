d56ff0d7d68430dd6b874b8949db2e7c27ab24b9
Osvaldo Urian <osvaldourian28@gmail.com>
2026-03-11 10:52:36 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000107564_osvaldourian/week6_a/Main.kt
+++ src/oop_00000107564_osvaldourian/week6_a/Main.kt
@@ -1,12 +1,12 @@
 package oop_00000107564_osvaldourian.week6_a
 
-// Fungsi ini Decoupled! Tidak peduli kelas aslinya apa.
 fun processCheckout(method: PaymentMethod, amount: Double) {
     println("-> Memulai checkout...")
-    method.pay(amount) // Dynamic polymorphism in action
+    method.pay(amount)
 }
 
 fun main() {
+    // === LATIHAN TERBIMBING ===
     val myWatch = Smartwatch()
     myWatch.showTime()
 
@@ -20,7 +20,21 @@ fun main() {
     processCheckout(method = pay1, amount = 50000.0)
     processCheckout(method = pay2, amount = 150000.0)
 
+    // === TUGAS MANDIRI SMART HOME ===
+    println("\n=== SMART HOME SYSTEM ===")
+
+    // Instansiasi perangkat
     val lamp = SmartLamp(id = "L001", name = "Ruang Tamu")
     val speaker = SmartSpeaker(id = "S001", name = "Google Nest Dapur")
     val cctv = SmartCCTV(id = "C001", name = "Ezviz Garasi")
+
+    // Instansiasi Hub dan tambahkan semua perangkat
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    // Test security mode dan matikan semua
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }
