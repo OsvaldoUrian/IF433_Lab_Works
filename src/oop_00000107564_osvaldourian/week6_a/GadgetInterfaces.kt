a93bccb49da5f39395ae566c7ff917f60fefb9ba
Osvaldo Urian <osvaldourian28@gmail.com>
2026-03-11 10:33:11 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000107564_osvaldourian/week6_a/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000107564_osvaldourian.week6_a
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
