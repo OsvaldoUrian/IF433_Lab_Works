797ec6e00f3c65567063e3f426c743fa701f0504
Osvaldo Urian <osvaldourian28@gmail.com>
2026-03-11 10:28:30 +0700

    week06: create compound interfaces

---
+++ src/oop_00000107564_osvaldourian/week6_a/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000107564_osvaldourian.week6_a
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
