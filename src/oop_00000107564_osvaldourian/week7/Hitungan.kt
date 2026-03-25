61e6bf856f4671683e6b88cf4b0c52b2ab3b04ed
Osvaldo Urian <osvaldourian28@gmail.com>
2026-03-25 10:12:04 +0700

    week07: create singleton DatabaseManager

---
+++ src/oop_00000107564_osvaldourian/week7/Hitungan.kt
@@ -0,0 +1,15 @@
+package oop_00000107564_osvaldourian.week7
+
+class Hitungan {
+    companion object {
+        }
+    fun pindah_halaman(namaHal:String) {
+        Hitungan(halaman = namaHal);
+    }}
+    init {
+         if (halaman == "home"){
+             println("welcome home");
+        }else {
+            println("hal lainnya")
+}
+}
---
+++ src/oop_00000107564_osvaldourian/week7/Koneksi.kt
@@ -0,0 +1,10 @@
+package oop_00000107564_osvaldourian.week7
+
+object Koneksi {
+    var alamatServer: String = "http://localhost:8080";
+
+    fun konek_db() {
+        println("Konek ke DB saya di $alamatServer");
+
+    }
+}
