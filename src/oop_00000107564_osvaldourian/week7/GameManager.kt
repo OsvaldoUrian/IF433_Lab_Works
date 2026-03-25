2da575fd25a70f6fc251128f301d09824db49eab
Osvaldo Urian <osvaldourian28@gmail.com>
2026-03-25 10:36:43 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_00000107564_osvaldourian/week7/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_00000107564_osvaldourian.week7
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
