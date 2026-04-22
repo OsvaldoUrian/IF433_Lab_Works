4d322dfcea9a1618954d630ef65b742e021ff263
Osvaldo Urian <osvaldourian28@gmail.com>
2026-04-22 19:50:03 +0700

    week09: (task) test full crypto trading data pipeline

--- src/oop_00000107564_osvaldourian/week7/MainProgram.kt
+++ src/oop_00000107564_osvaldourian/week7/MainProgram.kt
@@ -1,15 +1,4 @@
 package oop_00000107564_osvaldourian.week7
 
 
-    fun main() {
-        Koneksi.alamatServer = "localhost";
-        println("server:" + Koneksi.alamatServer);
-        Koneksi.konek_db();
 
-        println("kampus: " + Hitungan.namaKampus);
-        println("NIM: " + Hitungan.noIndukKampus);
-        println("tambahan: " + Hitungan.penambahan(a=10, b=20));
-
-        Hitungan.pindah_halaman()
-
-    }
