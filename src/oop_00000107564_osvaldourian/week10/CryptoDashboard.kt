818f8cb33c33c9e42bbae642339fb0e7a76484db
Osvaldo Urian <osvaldourian28@gmail.com>
2026-05-01 13:02:48 +0700

    week10: (task) populate coin repository

--- src/oop_00000107564_osvaldourian/week10/CryptoDashboard.kt
+++ src/oop_00000107564_osvaldourian/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000107564_osvaldourian.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.3))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
