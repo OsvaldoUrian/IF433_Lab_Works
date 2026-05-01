c533bfedb9f4d2478c86f77bb256870a266248e5
Osvaldo Urian <osvaldourian28@gmail.com>
2026-05-01 13:05:38 +0700

    week10: (task) test full generic crypto architecture

--- src/oop_00000107564_osvaldourian/week10/GenericMethods.kt
+++ src/oop_00000107564_osvaldourian/week10/GenericMethods.kt
@@ -2,8 +2,8 @@ package oop_00000107564_osvaldourian.week10
 
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
+}
 
-    fun <T> processData(input: T): T {
-        return input
-    }
+fun <T> processData(input: T): T {
+    return input
 }
