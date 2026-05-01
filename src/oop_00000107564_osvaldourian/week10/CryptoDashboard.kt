1a45363ddd63403a8b465f8f8d6e2efc20314274
Osvaldo Urian <osvaldourian28@gmail.com>
2026-05-01 13:04:05 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000107564_osvaldourian/week10/CryptoDashboard.kt
+++ src/oop_00000107564_osvaldourian/week10/CryptoDashboard.kt
@@ -13,4 +13,14 @@ fun main() {
     response.data.forEach { coin ->
         println("Coin: ${coin.name}, Balance: ${coin.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX001", 500.0))
+    txRepo.add(Transaction("TX002", 1200.5))
+    txRepo.add(Transaction("TX003", 75.25))
+
+    println("\n=== TRANSACTIONS ===")
+    txRepo.getAll().forEach { tx ->
+        println("ID: ${tx.id}, Amount: ${tx.amount}")
+    }
 }
