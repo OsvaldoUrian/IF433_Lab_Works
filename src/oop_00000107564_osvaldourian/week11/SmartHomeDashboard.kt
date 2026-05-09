050eb60bc3caa3f07a23251a8e7caa81937c3424
Osvaldo Urian <osvaldourian28@gmail.com>
2026-05-09 20:02:14 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000107564_osvaldourian/week11/SmartHomeDashboard.kt
+++ src/oop_00000107564_osvaldourian/week11/SmartHomeDashboard.kt
@@ -2,4 +2,12 @@ package oop_00000107564_osvaldourian.week11
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
+
+    // Konfigurasi Pencahayaan
+    SmartDevice("Philips WiZ Living Room", "Lighting").apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
 }
