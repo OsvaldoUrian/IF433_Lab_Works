d96ea8f4d3b330664abfa9f42255544e2239f278
Osvaldo Urian <osvaldourian28@gmail.com>
2026-05-09 20:05:59 +0700

    week11: (task) calculate total power usage

--- src/oop_00000107564_osvaldourian/week11/SmartHomeDashboard.kt
+++ src/oop_00000107564_osvaldourian/week11/SmartHomeDashboard.kt
@@ -40,4 +40,8 @@ fun main() {
     with(homeDevices) {
         println("Total perangkat terdaftar: ${this.size}")
     }
+
+    // Kalkulasi Daya dengan run
+    val totalPower = homeDevices.run { sumOf { it.powerLoad } }
+    println("Total konsumsi daya: $totalPower Watt")
 }
