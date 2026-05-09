ca742147eeef3be39e18c4a9c890dd055328cc6e
Osvaldo Urian <osvaldourian28@gmail.com>
2026-05-09 20:05:32 +0700

    week11: (task) format dashboard output using with

--- src/oop_00000107564_osvaldourian/week11/SmartHomeDashboard.kt
+++ src/oop_00000107564_osvaldourian/week11/SmartHomeDashboard.kt
@@ -34,4 +34,10 @@ fun main() {
     searchResult?.let {
         println(it.diagnose())
     }
+
+    // Format Summary dengan with
+    println("\n=== DASHBOARD SUMMARY ===")
+    with(homeDevices) {
+        println("Total perangkat terdaftar: ${this.size}")
+    }
 }
